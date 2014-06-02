cc995a98e35df57ac99b216eb9841faa53aa7973
George Knee <georgeknee@googlemail.com>
2014-06-02 11:09:54 +0100

    reflowed app.rb and wrote helper functions. working site

--- helper_functions.rb
+++ helper_functions.rb
@@ -8,8 +8,14 @@
 #  event_name('ladies_april') #=> "Ladies' Monthly Triathlon"
 #
 #  [Extension: get it to work for any code e.g. men_december or men_xx]
-def event_name
-
+def event_name(event_code)
+	if	event_code.split('_').first == 'men'
+		"Men's Monthly Triatlon"
+	else
+		if event_code.split('_').first == 'ladies'
+			"Ladies' Monthly Triathlon"
+		end
+	end
 end
 
 # Constructs the event date from its code
@@ -20,8 +26,11 @@ end
 #  event_date('ladies_april') #=> '15/4/2014'
 #
 # You can assume the year is 2014 and the triathlon is always on the 15th.
-def event_date
-
+def event_date(event_code)
+	month = event_code.split('_').last
+	lookup = Hash['january',1,'february',2,'march',3,'april',4,'may',5,'june',6,'july',7,'august',8,'september',9,'october',10,'november',11,'december',12]
+	month_number = lookup[month]
+	"15/#{month_number}/2014"
 end
 
 # Extracts the first name from a full name.
@@ -30,7 +39,7 @@ end
 #   first_name('Alan Jones') #=> 'Alan'
 #
 def first_name(full_name)
-
+	full_name.split(' ').first
 end
 
 
@@ -42,6 +51,10 @@ end
 #   [Extension]
 #   last_name('Pierre de Fermat') #=> 'de Fermat'
 #
-def second_name(full_name)
-
+def last_name(full_name)
+	if full_name == ""
+		""
+	else
+		full_name.split[1..-1].join(' ')
+	end
 end
